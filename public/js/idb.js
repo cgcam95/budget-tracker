c0880c5ef612057eb84a986242c2087881d9e83d
Camil Grabowski <camil.grabowski@uconn.edu>
2021-10-12 14:35:30 -0400

    finished adding service workers

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,7 +5,7 @@ const request = indexedDB.open("budget_tracker", 1);
 
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
-  db.creatObjectStore('budget_tracker', { autoIncrement: true });
+  db.createObjectStore('budget_tracker', { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
